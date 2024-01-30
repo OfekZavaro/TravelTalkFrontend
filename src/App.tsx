aff2912441bdca7f6c798e95fb164e04d2d1ca36
Adi Mor <adimor87@gmail.com>
2024-01-30 15:58:31 +0200

    implement Auth Page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,28 @@
 import React from "react";
 import HomePage from "./pages/HomePage";
+import AuthForm from "./components/AuthForm/AuthForm";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import LogInForm from "./components/AuthForm/LogInForm";
 
 const App = () => {
   return (
-    <div>
-      <HomePage />
-      {/* Add other components/pages as needed */}
-    </div>
+    <Router>
+      <div>
+        {/* Define your routes */}
+        <Routes>
+          <Route path="/" element={<AuthForm />} />
+          <Route path="/signUp" element={<AuthForm />} />
+          <Route path="/logIn" element={<LogInForm />} />
+          <Route path="/home" element={<HomePage />} />
+
+          {/* Add other routes as needed */}
+        </Routes>
+      </div>
+    </Router>
   );
 };
 
